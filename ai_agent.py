ed503acdf89d008cff21750ae110a09e54ac6d9e
Priyansh Singh <priyanshsingh855@gmail.com>
2026-01-08 13:40:26 +0530

    updated

--- ai_agent.py
+++ ai_agent.py
@@ -1,46 +1,52 @@
-from dotenv import load_dotenv
-load_dotenv()
-
-import os
-
-from langchain_groq import ChatGroq
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_community.tools.tavily_search import TavilySearchResults
 from langgraph.prebuilt import create_react_agent
-from langchain_core.messages.ai import AIMessage
+from langchain_core.messages import AIMessage
 
 
-# LLMs
-groq_llm = ChatGroq(model="llama-3.3-70b-versatile")
+def get_response_from_ai_agent(llm_id, query, allow_search, system_prompt, provider):
+    # -------------------------
+    # LLM Selection
+    # -------------------------
+    if provider == "Gemini":
+        llm = ChatGoogleGenerativeAI(
+            model=llm_id,
+            temperature=0.3
+        )
+    else:
+        return "Invalid provider. Use 'Gemini'"
 
-gemini_llm = ChatGoogleGenerativeAI(
-    model="gemini-1.5-flash",
-    temperature=0.3
-)
+    # -------------------------
+    # Tools
+    # -------------------------
+    tools = [TavilySearchResults(max_results=2)] if allow_search else []
 
-# Tool
-search_tool = TavilySearchResults(max_results=4)
+    # -------------------------
+    # Agent (Prebuilt LangGraph)
+    # -------------------------
+    agent = create_react_agent(
+        model=llm,
+        tools=tools,
+        state_modifier=system_prompt
+    )
 
-# System prompt
-system_prompt = "You are an AI chatbot that acts smart, friendly, and helpful."
+    # -------------------------
+    # State (Memory)
+    # -------------------------
+    state = {
+        "messages": query
+    }
 
-# Agent
-agent = create_react_agent(
-    model=groq_llm,
-    tools=[search_tool],
-    state_modifier=system_prompt
-)
- 
-# Query
-query = "Tell me about the crypto market in 2024."
+    # -------------------------
+    # Invoke Agent
+    # -------------------------
+    response = agent.invoke(state)
 
-state = {
-    "messages": [
-        ("user", query)
+    messages = response.get("messages")
+    ai_messages = [
+        message.content
+        for message in messages
+        if isinstance(message, AIMessage)
     ]
-}
 
-response = agent.invoke(state)
-messages=response.get("messages")
-ai_messages=[message.content for message in messages if isinstance(message, AIMessage)]
-print(ai_messages[-1])
+    return ai_messages[-1]
